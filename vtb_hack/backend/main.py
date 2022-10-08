3d218e36f4f7516a0dbb19c844178d41934a5fcb
ksetherg <ksetherg@gmail.com>
2022-10-08 23:50:03 +0200

    Fix

--- vtb_hack/backend/main.py
+++ vtb_hack/backend/main.py
@@ -1,2 +1,22 @@
+from typing import Dict, List, Any
 from vtb_hack.backend.ch_client import ClickHouse
 
+
+ch_client = ClickHouse('prod_cluster')
+
+
+def get_news_records(label: str, limit: int = 100) -> List[Dict[str, Any]]:
+    q = f"""
+        SELECT *
+        FROM news.rcc_news
+        WHERE label = '{label}'
+        ORDER BY date DESC
+        LIMIT {limit}
+    """
+    records = ch_client.select_as_records(q)
+    return records
+
+
+if __name__ == "__main__":
+    records = get_news_records('Юристы')
+    print(records[0])
