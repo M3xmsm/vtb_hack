248a212526c2fe3f295a07e981737f97d5f40e14
ksetherg <ksetherg@gmail.com>
2022-10-09 00:44:24 +0200

    Fix

--- vtb_hack/backend/backbone.py
+++ vtb_hack/backend/backbone.py
@@ -1,11 +1,12 @@
 from typing import Dict, List, Any
+import random
 from vtb_hack.backend.ch_client import ClickHouse
 
 
 ch_client = ClickHouse('prod_cluster')
 
 
-def get_news_records_by_label(label: str, limit: int = 100) -> List[Dict[str, Any]]:
+def get_news_records_by_label(label: str, limit: int = 100, sample_size: int = 3) -> List[Dict[str, Any]]:
     q = f"""
         SELECT *
         FROM news.rcc_news
@@ -14,4 +15,5 @@ def get_news_records_by_label(label: str, limit: int = 100) -> List[Dict[str, An
         LIMIT {limit}
     """
     records = ch_client.select_as_records(q)
-    return records
+    sampled = random.choices(records, k=sample_size)
+    return sampled
